44f7aa49b0e6765ee1bc9f781a067ce25eb9955b
Fernando <fernandocajaschuqui@hotmail.com>
2025-03-27 16:34:53 -0500

    Modificacion de diseño en la pagina

--- src/app/multimedia/multimedia.component.ts
+++ src/app/multimedia/multimedia.component.ts
@@ -12,6 +12,15 @@ export class MultimediaComponent {
     'https://www.t-systems.com/resource/image/1007596/ratio3x2/1440/960/e74e1dbd56ae03ec1dfde7dc26b634d3/70C7C7845E345A2EC2B3E79C3F4631D1/im-quantum-computing-use-cases.jpg',
     'https://s1.elespanol.com/2024/08/17/enclave-ods/historias/878922213_248531354_1706x1280.jpg',
     'https://i.ytimg.com/vi/n51issoMs2A/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBhr-MYDhAEFT3ayaya_oCaphjxQQ',
-    'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2021-12/Telosa-ciudad-futurista-b2d97c.jpg'
+    'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2021-12/Telosa-ciudad-futurista-b2d97c.jpg',
+    'https://www.t-systems.com/resource/image/1007666/ratio3x2/1440/960/e74e1dbd56ae03ec1dfde7dc26b634d3/CDFAFB848ABF9B560A76783174EB2F35/im-quantum-computing-use-cases.jpg',
+    'https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/32060cbfaae0274be2394aefcfc35e4f.jpg',
+    'https://assets.xboxservices.com/assets/20/38/203850f5-1bed-4912-b25f-193ee890c97f.jpg?n=Fortnite_GLP-Page-Hero-1084_876951_1920x1080.jpg'
   ];
+
+  selectedImage: string | null = null;
+
+  selectImage(image: string) {
+    this.selectedImage = image;
+  }
 }
